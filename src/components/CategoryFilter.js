5bdc729141637898d92c8b46a0b3ad545c94f377
MW1R1 <nickmwirigi58@gmail.com>
2023-04-26 00:11:16 +0300

    completed assignment

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,38 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter ({ categories, selectedCategory, onCategoryClick }) {
+  const categoryButtons = categories.map((category) => {
+    const className = category === selectedCategory ? "selected" : "";
+function CategoryFilter({ categories, selectedCategory, onSelectCategory }) {
+  // console.log(categories)
+  const displayCategories = categories.map((category) => {
+    const className = category === selectedCategory ? "selected" : null;
+    return (
+      <button
+        key={category}
+        className={className}
+        onClick={() => onCategoryClick(category)}
+      >
+        {category}
+      <button key={category} className={className} onClick={() => onSelectCategory(category)}>{category}
+      </button>
+    );
+    )
+  });
+  return  (<div className="categories">
+    <h5>Category filters</h5>
+    {categoryButtons}
+    </div>);
+};
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {displayCategories}
     </div>
   );
 }
 
+
+export default CategoryFilter;
 export default CategoryFilter;
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,30 @@
 import React from "react";
+// import { TASKS } from "../data";
 
-function Task() {
+function Task ({ task, onDeleteTask }) {
+  const handleDeleteClick = () => {
+    onDeleteTask(task.id);
+  };
+
+function Task({text, category, onDeleteTask}) {
+  // console.log(text)
+  // console.log(category)
+  function handleClick(){
+    onDeleteTask(text);
+  }
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{task.text}</div>
+      <div className="text">{task.category}</div>
+      <button className="delete" onClick={handleDeleteClick}>X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={handleClick}>X</button>
     </div>
   );
+};
+
+
 }
 
 export default Task;
